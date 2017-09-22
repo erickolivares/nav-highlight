4a32bc7c1443c37846c706a4c0d9d2ebce764a90
Erick Olivares <erickolivares21@gmail.com>
2017-09-22 00:10:50 -0700

    git init

---
+++ read.md
@@ -0,0 +1,31 @@
+# Highlight Nav
+
+This Jquery plugin allows you to scroll through a page while highlighting list-items on your nav
+
+## Select your nav list items 
+
+On line 5, choose the class of the elements that contain a tags with the `hrefs` attribute. 
+
+Example: `nav li`
+
+```
+<nav>
+	<li>
+		<a href="section1"></a>
+	</li>
+</nav>
+
+```
+
+## Select the sections 
+
+On line 6, choose the elements of the sections that contain matching ID's
+
+Example: `section`
+
+```
+<section id="section1">
+	...
+</section>
+
+```
---
+++ script.js
@@ -0,0 +1,63 @@
+jQuery(document).ready(function( $ ) {
+
+	widthFullScreen();
+
+	var navItems = $("nav li"); //nav list items
+	var modId = $("section"); //divs with ID's
+	
+
+	var points = [];
+
+	for (var i = 0; i < modId.length; i++) {
+		var thismod = modId.eq([i]);
+		var thismodID = thismod.attr("id")
+		for (var j = 0; j < navItems.length; j++) {
+			var thisnavhref = navItems.find('a').eq([j]).attr("href").slice(1);
+			if(thismodID == thisnavhref) {
+				points.push([[j],thismod.offset().top]);
+			}
+		}
+	}
+
+	function lookup(location) {
+		
+		var offset = $(window).height() * .2;
+		var activeLocation = offset + location;
+
+		for (var x = 0; x < points.length; x++) {
+
+			var nextItem = x + 1;
+
+			if( points[x]  === 0) {
+				var currectItemValue = 0;
+			} else {
+				var currectItemValue = points[x][1];
+			}
+
+			if(points.length + 1 === nextItem + 1) {
+				var nextItemValue = 10000000;
+			} else {
+				var nextItemValue = points[nextItem][1];
+			}
+
+			if(currectItemValue < activeLocation &&  activeLocation < nextItemValue) {
+				navItems.eq(points[x][0]).addClass('active');
+			} else {
+				navItems.eq(points[x][0]).removeClass('active');
+			}
+		}
+	};
+
+	window.onscroll = function() {
+		var currentpos = $(document).scrollTop();
+		lookup(currentpos);
+	};
+
+	window.onresize = function() {
+		widthFullScreen();
+		var currentpos = $(document).scrollTop();
+		lookup(currentpos);
+	}
+
+
+});
